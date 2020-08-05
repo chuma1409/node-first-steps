023076547c12ee89951322fb649d7831470b6ff3
chuma1409 <chumagushman14@gmail.com>
2020-08-05 11:12:54 +0200

    initial commit

---
+++ greet_xola.js
@@ -0,0 +1,16 @@
+const chalk = require('chalk');
+//import the greet module that is in the current folder
+const greet = require('./greet');
+
+const styledMessage = chalk.bgGreen.black(greet('Xola'));
+console.log(styledMessage)
+var figlet = require('figlet');
+ 
+figlet('Hello, Xola', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log( chalk.bgYellowBright.black(data))
+});
